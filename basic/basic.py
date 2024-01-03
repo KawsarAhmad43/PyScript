d154cfecb4bb1780fe6ef718d7c2bb2c0089b130
MD. Kawsar Ahmad <54704888+KawsarAhmad43@users.noreply.github.com>
2024-01-03 18:33:43 +0600

    Create basic.py

---
+++ basic/basic.py
@@ -0,0 +1,35 @@
+# print
+print("Hello World!")
+
+# take Input
+name = input('Enter your name:')
+# print variable and concate
+print('Hello, ' + name)
+
+# variables
+
+#variables are case sensitive
+x=1
+print(type(x))
+y="1"
+print(type(y))
+z= True
+print(type(z))
+
+
+#Type Casting
+a= "43"
+print(type(a))
+b= int(a)
+print(type(b))
+
+
+## Legal Variable Names
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+#Python uses indentation to indicate a block of code.
